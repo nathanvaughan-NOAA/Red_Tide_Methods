4af3e8af869a46392a7e6cca871bcff5042d86e3
norellia-NOAA <alex.norelli@noaa.gov>
2026-07-01 13:34:45 -0400

    Add none EM for no_rt scenarios

--- misc/adjust_base_models_bycatch_fleet.R
+++ misc/adjust_base_models_bycatch_fleet.R
@@ -119,3 +119,32 @@ inputs$dat <- dat
 r4ss::SS_write(inputs, dir = new_mod_path, overwrite = TRUE)
 
 SSMSE::run_ss_model(new_mod_path)
+
+
+# Initial model to modify
+mod_path <- file.path("base_models", "flat")
+
+##### None Model #####
+
+# Create a new directory to put a new, modified version of the model
+new_mod_path <- file.path("base_models", "none")
+
+# Copy default_SigmaR to Emperical_new folder
+copy_SS_inputs(dir.old = mod_path, dir.new = new_mod_path)
+
+# Load SS files
+inputs <- r4ss::SS_read(dir = new_mod_path)
+dat <- inputs$dat
+
+# Change the by_catch fleet info
+
+dat$bycatch_fleet_info$Fmult <- 3
+dat$bycatch_fleet_info$F_or_first_year <- 2015
+dat$bycatch_fleet_info$F_or_last_year <- 2017
+
+inputs$dat <- dat
+
+# Write file
+r4ss::SS_write(inputs, dir = new_mod_path, overwrite = TRUE)
+
+SSMSE::run_ss_model(new_mod_path)
